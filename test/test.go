92dc200830f048d19f149b2d9c995935f19d8885
Gert Drapers <gert.drapers@live.com>
2022-09-07 23:58:25 -0700

    update test service

--- test/test.go
+++ test/test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/aserto-dev/go-directory/aserto/directory/exporter/v2"
 	"github.com/aserto-dev/go-directory/aserto/directory/importer/v2"
 	"github.com/aserto-dev/go-directory/aserto/directory/v2"
+	"github.com/aserto-dev/go-directory/aserto/directory/writer/v2"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -16,6 +17,7 @@ const port = 12345
 
 type server struct {
 	directory.UnimplementedDirectoryServer
+	writer.UnimplementedWriterServer
 	importer.UnimplementedImporterServer
 	exporter.UnimplementedExporterServer
 }
@@ -28,6 +30,7 @@ func main() {
 	s := grpc.NewServer()
 
 	directory.RegisterDirectoryServer(s, &server{})
+	writer.RegisterWriterServer(s, &server{})
 	exporter.RegisterExporterServer(s, &server{})
 	importer.RegisterImporterServer(s, &server{})
 
