b4ae05b935a3c3bd258353dacef5c099135965ab
Ronen Hilewicz <ronen@aserto.com>
2024-02-14 14:30:34 -0500

    Add ErrNotImplemented

--- pkg/derr/errors.go
+++ pkg/derr/errors.go
@@ -46,4 +46,5 @@ var (
 	ErrProtoValidate                = cerr.NewAsertoError("E20050", codes.InvalidArgument, http.StatusBadRequest, "")
 	ErrGraphDirectionality          = cerr.NewAsertoError("E20052", codes.InvalidArgument, http.StatusPreconditionFailed, "unable to determine graph directionality")
 	ErrUnknownOpCode                = cerr.NewAsertoError("E20053", codes.InvalidArgument, http.StatusPreconditionFailed, "unknown import OpCode value")
+	ErrNotImplemented               = cerr.NewAsertoError("E20054", codes.Unimplemented, http.StatusNotImplemented, "not implemented")
 )
