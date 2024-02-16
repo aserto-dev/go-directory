e98349ef4b16da71dec340a6a55f65b0e86042e8
Ronen Hilewicz <ronen@aserto.com>
2024-02-16 14:03:23 -0500

    ErrNotImplemented + ErrCycleDetected (#44)
    * Add ErrNotImplemented
    
    * Add ErrCycleDetected
    
    * Remove references to v3.ObjectDependency
    
    It is no longer defined.

--- pkg/derr/errors.go
+++ pkg/derr/errors.go
@@ -46,4 +46,6 @@ var (
 	ErrProtoValidate                = cerr.NewAsertoError("E20050", codes.InvalidArgument, http.StatusBadRequest, "")
 	ErrGraphDirectionality          = cerr.NewAsertoError("E20052", codes.InvalidArgument, http.StatusPreconditionFailed, "unable to determine graph directionality")
 	ErrUnknownOpCode                = cerr.NewAsertoError("E20053", codes.InvalidArgument, http.StatusPreconditionFailed, "unknown import OpCode value")
+	ErrNotImplemented               = cerr.NewAsertoError("E20054", codes.Unimplemented, http.StatusNotImplemented, "not implemented")
+	ErrCycleDetected                = cerr.NewAsertoError("E20055", codes.FailedPrecondition, http.StatusPreconditionFailed, "cycle detected")
 )
