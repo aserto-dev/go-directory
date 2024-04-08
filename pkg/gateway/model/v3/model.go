c780aaf1f43a747cb79ad95379c1ab39ceec84bf
Gert Drapers <gert.drapers@live.com>
2024-04-08 09:42:15 -0700

    use grpc.NewClient

--- pkg/gateway/model/v3/model.go
+++ pkg/gateway/model/v3/model.go
@@ -26,7 +26,7 @@ const (
 )
 
 func RegisterModelStreamHandlersFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
-	conn, err := grpc.DialContext(ctx, endpoint, opts...)
+	conn, err := grpc.NewClient(endpoint, opts...)
 	if err != nil {
 		return err
 	}
