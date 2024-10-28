7f7554bb3d4d7eccd0587290e94dd1b7eb69d201
Gert Drapers <1533850+gertd@users.noreply.github.com>
2024-10-28 09:49:20 -0700

    id-msg

--- go.mod
+++ go.mod
@@ -23,6 +23,7 @@ require (
 	github.com/google/cel-go v0.21.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/oklog/ulid/v2 v2.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rs/zerolog v1.33.0 // indirect
 	github.com/stoewer/go-strcase v1.3.0 // indirect
--- pkg/validate/validate_test.go
+++ pkg/validate/validate_test.go
@@ -1,10 +1,13 @@
 package validate_test
 
 import (
+	"errors"
 	"testing"
 
 	dsc3 "github.com/aserto-dev/go-directory/aserto/directory/common/v3"
 	"github.com/bufbuild/protovalidate-go"
+	"github.com/oklog/ulid/v2"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -20,5 +23,38 @@ func TestCommonValidation(t *testing.T) {
 		t.Logf("validation failed: %s", err)
 	} else {
 		t.Log("validation succeeded")
+	}
+}
+
+func TestIdValidation(t *testing.T) {
+	v, err := protovalidate.New()
+	require.NoError(t, err)
+
+	msg := &dsc3.Id{Value: ulid.Make().Bytes()}
+
+	if err := v.Validate(msg); err != nil {
+		assert.NoError(t, err)
+	}
+
+	if err = v.Validate(msg); err != nil {
+		t.Logf("validation failed: %s", err)
+	} else {
+		t.Log("validation succeeded")
+	}
+}
+
+func TestId2Validation(t *testing.T) {
+	v, newErr := protovalidate.New()
+	require.NoError(t, newErr)
+
+	err := v.Validate(&dsc3.Id{Value: make([]byte, 17)})
+	assert.Error(t, err)
+
+	var valErr *protovalidate.ValidationError
+	assert.ErrorAs(t, err, &valErr)
+
+	if ok := errors.As(err, &valErr); ok {
+		pb := valErr.ToProto()
+		t.Log(pb.GetViolations())
 	}
 }
