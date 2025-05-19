e36989909ae3844f34879b3605ac2f876d1676ae
Gert Drapers <1533850+gertd@users.noreply.github.com>
2025-05-19 09:45:50 -0700

    disable manifest

--- pkg/manifest/metadata.go
+++ pkg/manifest/metadata.go
@@ -1,45 +1,45 @@
 package manifest
 
-import (
-	"context"
+// import (
+// 	"context"
 
-	"github.com/grpc-ecosystem/go-grpc-middleware/util/metautils"
-	"github.com/samber/lo"
-)
+// 	"github.com/grpc-ecosystem/go-grpc-middleware/util/metautils"
+// 	"github.com/samber/lo"
+// )
 
-const (
-	HeaderAsertoManifestRequest = "Aserto-Manifest-Request"
-	HeaderAsertoUpdatedAt       = "Aserto-Updated-At"
-)
+// const (
+// 	HeaderAsertoManifestRequest = "Aserto-Manifest-Request"
+// 	HeaderAsertoUpdatedAt       = "Aserto-Updated-At"
+// )
 
-type ManifestRequest string
+// type ManifestRequest string
 
-const (
-	// Return the manifest metadata and body.
-	ManifestRequestDefault ManifestRequest = ""
-	// Only return the manifest metadata.
-	ManifestRequestMetadataOnly ManifestRequest = "metadata-only"
-	// Only return the manifest metadata and model.
-	ManifestRequestModelOnly ManifestRequest = "model-only"
-	// Return the manifest metadata, body, and model.
-	ManifestRequestWithModel ManifestRequest = "with-model"
-)
+// const (
+// 	// Return the manifest metadata and body.
+// 	ManifestRequestDefault ManifestRequest = ""
+// 	// Only return the manifest metadata.
+// 	ManifestRequestMetadataOnly ManifestRequest = "metadata-only"
+// 	// Only return the manifest metadata and model.
+// 	ManifestRequestModelOnly ManifestRequest = "model-only"
+// 	// Return the manifest metadata, body, and model.
+// 	ManifestRequestWithModel ManifestRequest = "with-model"
+// )
 
-func IncomingManifestRequest(ctx context.Context) ManifestRequest {
-	md := metautils.ExtractIncoming(ctx)
-	amr := ManifestRequest(md.Get(HeaderAsertoManifestRequest))
+// func IncomingManifestRequest(ctx context.Context) ManifestRequest {
+// 	md := metautils.ExtractIncoming(ctx)
+// 	amr := ManifestRequest(md.Get(HeaderAsertoManifestRequest))
 
-	if !lo.Contains([]ManifestRequest{ManifestRequestMetadataOnly, ManifestRequestModelOnly, ManifestRequestWithModel}, amr) {
-		amr = ManifestRequestDefault
-	}
+// 	if !lo.Contains([]ManifestRequest{ManifestRequestMetadataOnly, ManifestRequestModelOnly, ManifestRequestWithModel}, amr) {
+// 		amr = ManifestRequestDefault
+// 	}
 
-	return amr
-}
+// 	return amr
+// }
 
-func (mr ManifestRequest) WithBody() bool {
-	return lo.Contains([]ManifestRequest{ManifestRequestDefault, ManifestRequestWithModel}, mr)
-}
+// func (mr ManifestRequest) WithBody() bool {
+// 	return lo.Contains([]ManifestRequest{ManifestRequestDefault, ManifestRequestWithModel}, mr)
+// }
 
-func (mr ManifestRequest) WithModel() bool {
-	return lo.Contains([]ManifestRequest{ManifestRequestModelOnly, ManifestRequestWithModel}, mr)
-}
+// func (mr ManifestRequest) WithModel() bool {
+// 	return lo.Contains([]ManifestRequest{ManifestRequestModelOnly, ManifestRequestWithModel}, mr)
+// }
